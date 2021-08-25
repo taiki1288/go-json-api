d2af638267d725173abd2bf1e5382b80c0d91d55
taiki yamashita <taikity0808@gmail.com>
2021-08-25 22:42:31 +0900

    feat: literal

--- main.go
+++ main.go
@@ -5,8 +5,25 @@ import (
 ) 
 
 type Task struct {
-	ID int `json:"id"`
-	Title string `json:"title"`
-	Content string `json:"content"`
+	ID      int       `json:"id"`
+	Title   string    `json:"title"`
+	Content string    `json:"content"`
 	DueDate time.Time `json:"due_date"`
 }
+
+var tasks = []Task{{
+	ID: 1,
+	Title: "A",
+	Content: "Aタスク",
+	DueDate: time.Now(),
+}, {
+	ID: 2,
+	Title: "B",
+	Content: "Bタスク",
+	DueDate: time.Now(),
+}, {
+	ID: 3,
+	Title: "C",
+	Content: "Cタスク",
+	DueDate: time.Now(),
+}}
